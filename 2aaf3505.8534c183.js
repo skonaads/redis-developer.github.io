3d066e9b31fb59fb8dc5fc48192fb35f95276ee7
Tugdual Grall <tugdual@gmail.com>
2020-12-31 12:01:58 +0100

    Deploy website - based on 10bb7a43efeadc7b5eafde648c6f6f149ded9f46

---
+++ 2aaf3505.8534c183.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{76:function(e){e.exports=JSON.parse('{"allTagsPath":"/redis-developer/blog/tags","slug":"facebook","name":"facebook","count":1,"permalink":"/redis-developer/blog/tags/facebook"}')}}]);
---
+++ 708c6667.61e0fc06.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{83:function(e){e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Welcome","permalink":"/redis-developer/blog/welcome"},{"title":"Hello","permalink":"/redis-developer/blog/hello-world"},{"title":"Hola","permalink":"/redis-developer/blog/hola"}]}')}}]);
